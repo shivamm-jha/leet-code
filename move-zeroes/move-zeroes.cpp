61f39d7a6dd8cb7af087a422547732ec0c4d6325
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2021-12-25 11:34:22 +0530

    Time: 24 ms (53.90%), Space: 19.5 MB (7.50%) - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,20 +1,48 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> v;
-        int count=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0)v.push_back(nums[i]);
-            else count++;
-        }
+//         brute force
+//         space complexity 0(n)...... time complexity 0(n)
         
-        for(int i=0;i<count;i++){
-            v.push_back(0);
-        }
         
-        for(int i=0;i<n;i++){
-            nums[i]=v[i];
-        }
+//         vector<int> v;
+//         int count=0;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             if(nums[i]!=0)v.push_back(nums[i]);
+//             else count++;
+//         }
+        
+//         for(int i=0;i<count;i++){
+//             v.push_back(0);
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             nums[i]=v[i];
+//         }
+        
+        
+        
+//         better solution 
+        
+//         space complxity 0(1) ..... time complexity 0(n)
+        
+        
+        
+//         int n=nums.size();
+//         int last_nonzero=0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]!=0){
+//                 nums[last_nonzero++]=nums[i];
+//             }
+//         }
+        
+//         for(int i=last_nonzero;i<n;i++){
+//             nums[i]=0;
+//         }
+        
+        
+         // stable_partition(nums.begin(), nums.end(), [](int n) {return n>0; });
+        stable_partition(begin(nums),end(nums),[](int i){return i;});
     }
 };
