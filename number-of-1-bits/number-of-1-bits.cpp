66913bf9f1e635ff93d9f75384863cf0e2edac77
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2021-12-31 00:49:55 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.40%) - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        return bitset<32>(n).count();
+        //return bitset<32>(n).count();
+        
+        int count=0,mask=1;
+       while(n>0){
+           if((n&mask)!=0)count++;
+           n=n>>1;
+       }
+        return count;
     }
 };
