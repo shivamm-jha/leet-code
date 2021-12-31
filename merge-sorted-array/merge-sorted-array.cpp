73ca5982693ae4e24a23b97e41b811d47f7d0c78
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2021-12-31 22:43:17 +0530

    Update merge-sorted-array.cpp

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-//         sort(nums1.begin(),nums1.end());
-//         sort(nums2.begin(),nums2.end());
+        //         brute force time complexity 0(nlogn)... space complexity 0(m+n)
+        
+        
 //         vector<int> v;
 //         for(int i=0;i<m;i++){
-//             if(nums1[i]!=0){
+           
 //             v.push_back(nums1[i]);
-//             }
+            
 //         }
 //         for(int i=0;i<n;i++){
-//              if(nums2[i]!=0)
+            
 //             v.push_back(nums2[i]);
 //         }
-//         //sort(v.begin(),v.end());
+      
 //         for(int i=0;i<v.size();i++){
             
 //             nums1[i]=v[i];
 //         }
 //         sort(nums1.begin(),nums1.end());
         
+        
+//         better solution time complexity 0(n)... space complexity 0(1)
        int i=m-1,j=n-1,k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
