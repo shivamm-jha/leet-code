27f27b372cbf7bf69394e99323ec10f6d8612aab
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-01-06 22:54:33 +0530

    Time: 4 ms (49.29%), Space: 8.3 MB (84.87%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        int n=nums.size();
-        list<int> g1;
-        for(int i=0;i<n;i++){
-            auto it=g1.begin();
-            for(int j=0;j<index[i];j++)it++;
-            g1.emplace(it,nums[i]);
-        }
-        vector<int> v;
-        list <int> :: iterator it;
-        for(it = g1.begin(); it != g1.end(); ++it)v.push_back(*it);
-        return v;
+        // int n=nums.size();
+        // list<int> g1;
+        // for(int i=0;i<n;i++){
+        //     auto it=g1.begin();
+        //     for(int j=0;j<index[i];j++)it++;
+        //     g1.emplace(it,nums[i]);
+        // }
+        // vector<int> v;
+        // list <int> :: iterator it;
+        // for(it = g1.begin(); it != g1.end(); ++it)v.push_back(*it);
+        // return v;
+        vector<int> result;
+        for(int i=0;i<index.size();++i)
+            result.insert(result.begin()+index[i],nums[i]);
+        return result;
     }
 };
