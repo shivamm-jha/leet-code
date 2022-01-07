ef1c5751450ef56fc2951d71cab844321c6aa409
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-01-08 00:05:16 +0530

    Time: 38 ms (10.60%), Space: 16.7 MB (48.28%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.cpp
+++ 382-linked-list-random-node/382-linked-list-random-node.cpp
@@ -11,21 +11,23 @@
 class Solution {
 public:
     ListNode * HeadNode;
+    int len=0;
     Solution(ListNode* head) {
         HeadNode = head;
+        ListNode * ptr=head;
+        while(ptr!=NULL){
+            ptr=ptr->next;
+            len++;
+        }
     }
     
     int getRandom() {
-        int res, len = 1;
-        ListNode* x = HeadNode;
-        while(x){
-            if(rand() % len == 0){
-                res = x->val;
-            }
-            len++;
-            x = x->next;
+        ListNode * ptr=HeadNode;
+        int index = rand()%len;
+        for(int i=0;i<index;i++){
+            ptr=ptr->next;
         }
-        return res;
+        return ptr->val;
     }
 };
 
