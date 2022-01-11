98bd3d5410947b9c3f6c522a8494c75f0caaf464
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-01-11 20:13:02 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (6.83%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,26 +11,44 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        vector<int> v;
-        ListNode * curr=head;
-        while(curr!=NULL){
-            v.push_back(curr->val);
-            curr=curr->next;
-        }
-        int n=v.size();
-        for(int i=0;i<n-1;i+=2){
-            swap(v[i],v[i+1]);
+//         vector<int> v;
+//         ListNode * curr=head;
+//         while(curr!=NULL){
+//             v.push_back(curr->val);
+//             curr=curr->next;
+//         }
+//         int n=v.size();
+//         for(int i=0;i<n-1;i+=2){
+//             swap(v[i],v[i+1]);
             
-        }
+//         }
         
-        ListNode * new_node = new ListNode(0); curr=new_node;
-        for(int i=0;i<n;i++){
-            ListNode * temp = new ListNode(v[i]);
-            curr->next = temp;
-            curr=temp;
-        }
+//         ListNode * new_node = new ListNode(0); curr=new_node;
+//         for(int i=0;i<n;i++){
+//             ListNode * temp = new ListNode(v[i]);
+//             curr->next = temp;
+//             curr=temp;
+//         }
         
-        return new_node->next;
+//         return new_node->next;
+        
+        
+       ListNode * new_head = new ListNode(0);
+       new_head->next = head;
+        ListNode * prev = new_head, * curr = head;
+        if(head==NULL || head->next==NULL)return head;
+        while(prev->next && curr->next){
+            prev->next = curr->next;
+            curr->next = curr->next->next;
+            prev->next->next = curr;
+            prev = curr;
+            curr = curr->next;
+        } 
+        
+        return new_head->next;
+            
+            
+        
         
         
     }
