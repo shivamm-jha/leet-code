cbd80bca9024977ba995613d5ca5fa339dcdadc2
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-01-14 01:04:32 +0530

    Time: 444 ms (51.24%), Space: 89.7 MB (63.37%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -8,13 +8,18 @@ public:
         int n = points.size();
         int count=1,end=points[0][1];
         for(int i=1;i<n;i++){
-            while(points[i][0]<=end){
-                end = min(points[i][1],end);
-                i++;
-                if(i>=n)return count;
+            // while(points[i][0]<=end){
+            //     end = min(points[i][1],end);
+            //     i++;
+            //     if(i>=n)return count;
+            // }
+            // count++;
+            // end = points[i][1];
+            if(points[i][0]<=end){continue; end = min(points[i][1],end);}
+            else{
+                count++;
+                end = points[i][1];
             }
-            count++;
-            end = points[i][1];
         }
         return count;
          
