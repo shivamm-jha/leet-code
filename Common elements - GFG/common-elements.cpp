66f213cbf1b983005a21716680176ddb94343d45
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-03 01:18:08 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -10,7 +10,7 @@ class Solution
        vector <int> commonElements (int a[], int b[], int c[], int n1, int n2, int n3)
         {
             vector<int> v;
-             set<int> st;
+            set<int> st;
             // for(int i=0;i<n1;i++){
             //     int flag1=0,flag2=0;
             //     for(int j=0;j<n2;j++){
@@ -35,37 +35,57 @@ class Solution
             // return v;
             
          
+        // for(int i=0;i<n1;i++){
+        //     int key = a[i];
+        //     int start = 0 , end = n2-1;
+        //     int flag1=0,flag2=0;
+        //     while(start<=end){
+        //         int mid = start + (end-start)/2;
+        //         if(b[mid]==key){
+        //             flag1=1; break;
+        //         }
+        //         else if(b[mid]> key) end=mid-1;
+        //         else start=mid+1;
+        //     }
+            
+        //     start=0, end=n3-1;
+        //     while(start<=end){
+        //         int mid = start + (end-start)/2;
+        //         if(c[mid]==key){
+        //             flag2=1; break;
+        //         }
+        //         else if(c[mid]> key) end=mid-1;
+        //         else start=mid+1;
+        //     }
+            
+        //     if(flag1 and flag2) st.insert(key);
+        // }
+        
+        // for(auto &i:st){
+        //     v.push_back(i);
+        // }
+        
+        // return v;
+        
+        
+        unordered_set<int> st1,st2;
+        set<int> st3;
+        for(int i=0;i<n2;i++)st1.insert(b[i]);
+        for(int i=0;i<n3;i++) st2.insert(c[i]);
         for(int i=0;i<n1;i++){
-            int key = a[i];
-            int start = 0 , end = n2-1;
-            int flag1=0,flag2=0;
-            while(start<=end){
-                int mid = start + (end-start)/2;
-                if(b[mid]==key){
-                    flag1=1; break;
-                }
-                else if(b[mid]> key) end=mid-1;
-                else start=mid+1;
+            if(st1.find(a[i])!=st1.end() and st2.find(a[i])!=st2.end()){
+                st3.insert(a[i]);
             }
-            
-            start=0, end=n3-1;
-            while(start<=end){
-                int mid = start + (end-start)/2;
-                if(c[mid]==key){
-                    flag2=1; break;
-                }
-                else if(c[mid]> key) end=mid-1;
-                else start=mid+1;
-            }
-            
-            if(flag1 and flag2) st.insert(key);
         }
         
-        for(auto &i:st){
+        for(auto &i:st3){
             v.push_back(i);
         }
-        
         return v;
+        
+        
+        
+        
             
         }
 
