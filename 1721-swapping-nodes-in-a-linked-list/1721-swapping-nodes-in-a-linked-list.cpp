c905022f95e5dbdbf47ba0832e49c0f52bf549ea
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-16 20:05:52 +0530

    Time: 980 ms (43.24%), Space: 180 MB (90.81%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -11,20 +11,50 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        vector<int> v;
-        ListNode * curr = head;
-        while(curr!=NULL){
-            v.push_back(curr->val);
-            curr = curr->next;
+//         vector<int> v;
+//         ListNode * curr = head;
+//         while(curr!=NULL){
+//             v.push_back(curr->val);
+//             curr = curr->next;
+//         }
+        
+//         swap(v[k-1], v[v.size()-k]);
+        
+//         curr = head;
+//         for(auto &i:v){
+//             curr->val = i;
+//             curr = curr->next;
+//         }
+//         return head;
+        
+        
+//      without using extra space.....
+        
+        
+        int size = 0;
+        ListNode * curr1 = head , *curr2 = head;
+        while(curr1!=NULL){
+            size++;
+            curr1 = curr1->next;
         }
         
-        swap(v[k-1], v[v.size()-k]);
-        
-        curr = head;
-        for(auto &i:v){
-            curr->val = i;
-            curr = curr->next;
+        curr1 = head;
+        int count1 =0 , count2 =0;
+        while(1){
+            if(count1<k-1){
+                count1++;
+                curr1 = curr1->next;
+            }
+            if(count2< size-k){
+                count2++;
+                curr2 = curr2->next;
+            }
+            if(count1==k-1 and count2==size-k)break;
         }
+        
+        swap(curr1->val, curr2->val);
         return head;
+       
+        
     }
 };
