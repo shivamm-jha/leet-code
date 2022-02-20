d2c9cb2d0548d19a63155caa5d4dc07100a42998
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-20 17:40:31 +0530

    Time: 36 ms (48.25%), Space: 11.7 MB (26.75%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+     static bool comp(pair<int,int> & a , pair<int,int> &b){
+        if(a.first==b.first) return a.second> b.second;
+       return  a.first < b.first;
+    }
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+         vector<pair<int,int>> v;
+        for(int i=0;i<intervals.size();i++){
+            v.push_back({intervals[i][0], intervals[i][1]});
+        }
+        
+        sort(v.begin(),v.end(),comp);
+        int count =0;
+        int start = v[0].first, end = v[0].second;
+        
+        for(int i=1;i<v.size();i++){
+            while(i<v.size() && v[i].first>=start && v[i].second<=end ){
+                count++;
+               
+               i++;
+            }
+            
+            if(i<v.size())start = v[i].first, end = v[i].second;
+        }
+        return v.size()-count;
+    }
+};
