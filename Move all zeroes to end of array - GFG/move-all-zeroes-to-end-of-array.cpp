87ba56f81c4ef3895957f6f17a04192d9160b67d
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-21 12:28:31 +0530

    Added solution - LeetHub

--- Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
@@ -30,16 +30,29 @@ public:
 	   //better solution
 	   //time complexity 0(n)... space complexity 0(1)
 	   
-	   int i=0,j=1;
-	   while(i<n && j<n){
-	       if(arr[i]==0 && arr[j]!=0){
-	           swap(arr[i],arr[j]);
-	           i++; j++;
+	   //int i=0,j=1;
+	   //while(i<n && j<n){
+	   //    if(arr[i]==0 && arr[j]!=0){
+	   //        swap(arr[i],arr[j]);
+	   //        i++; j++;
+	   //    }
+	   //    else if(arr[i]==0 && arr[j]==0){
+	   //        j++;
+	   //    }
+	   //    else i++, j++;
+	   //}
+	   
+	   // other method
+	   
+	   int lastNonZeroFound =0;
+	   for(int i=0;i<n;i++){
+	       if(arr[i]!=0){
+	           swap(arr[lastNonZeroFound++],arr[i]);
 	       }
-	       else if(arr[i]==0 && arr[j]==0){
-	           j++;
-	       }
-	       else i++, j++;
+	   }
+	   
+	   for(int i=lastNonZeroFound; i<n;i++){
+	       arr[i]=0;
 	   }
 	   
 	    
