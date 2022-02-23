953f84e165712fd021845eaba568989621cf1422
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-23 18:04:14 +0530

    Time: 4 ms (81.33%), Space: 7.1 MB (23.31%) - LeetHub

---
+++ 859-buddy-strings/859-buddy-strings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.size()!=goal.size()) return false;
+        int count =0;
+        vector<int> freq1(26,0),freq2(26,0);
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=goal[i])count++;
+            freq1[s[i]-'a']++;
+            freq2[goal[i]-'a']++;
+        }
+        
+        // cout<<count<<endl;
+        // for(int i=0;i<26;i++)cout<<freq1[i]<<" ";
+        // cout<<endl;
+        // for(int i=0;i<26;i++)cout<<freq2[i]<<" ";
+        // cout<<endl;
+        
+        if(count==2 && freq1==freq2)return true;
+        if(count==0 && freq1==freq2){
+            int flag=0;
+            for(int i=0;i<26;i++){
+                if(freq1[i]>=2){
+                    flag=1; break;
+                }
+            }
+            
+            if(flag)return true;
+        }
+       
+        return false;
+    }
+};
