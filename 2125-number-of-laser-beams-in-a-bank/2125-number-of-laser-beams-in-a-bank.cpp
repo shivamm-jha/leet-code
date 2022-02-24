6124ee03da76692d3b039bea1087dd4b6f5f9793
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-24 13:25:47 +0530

    Time: 167 ms (42.16%), Space: 23 MB (74.37%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,31 +1,50 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
+        
+//         time complexity 0(n* ( m+ (n*m) ))
         int n = bank.size(),m=bank[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int count1 =0, flag=0,count2=0;
-            for(int j=0;j<m;j++){
-                count1+= bank[i][j]-'0';
-            }
+//         int ans=0;
+//         for(int i=0;i<n;i++){
+//             int count1 =0, flag=0,count2=0;
+//             for(int j=0;j<m;j++){
+//                 count1+= bank[i][j]-'0';
+//             }
             
-            if(count1>0){
-                for(int k=i+1;k<n;k++){
-                    for(int l=0;l<m;l++){
-                        count2+=bank[k][l]-'0';
-                    }
-                    if(count2>0){
-                        ans+=count1*count2;
-                        break;
-                    }
-                     i=k;
-                }
+//             if(count1>0){
+//                 for(int k=i+1;k<n;k++){
+//                     for(int l=0;l<m;l++){
+//                         count2+=bank[k][l]-'0';
+//                     }
+//                     if(count2>0){
+//                         ans+=count1*count2;
+//                         break;
+//                     }
+//                      i=k;
+//                 }
                 
                
-            }
+//             }
             
-        }
+//         }
         
+//         return ans;
+        
+        int ans=0;
+        int pSecurityDevicefreq = 0;
+       
+        for(int i=0;i<n;i++){
+             int cSecurityDevicefreq = 0;
+            for(int j=0;j<m;j++){
+                if(bank[i][j]=='1'){
+                    cSecurityDevicefreq++;
+                }
+            }
+            if(cSecurityDevicefreq>0){
+                ans+= cSecurityDevicefreq * pSecurityDevicefreq;
+                pSecurityDevicefreq = cSecurityDevicefreq;
+            }
+        }
         return ans;
     }
 };
