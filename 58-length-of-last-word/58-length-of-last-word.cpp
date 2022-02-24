f962481c98da1e3618b13206a666d0e2e473ce28
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-02-24 12:35:37 +0530

    Time: 4 ms (46.25%), Space: 6.5 MB (52.58%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -3,16 +3,21 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.size();
         int i = n-1;
-        while(i>=0 && s[i]==' '){
-            i--;
-        }
-        int j= i;
-        
-        while(j>=0 && s[j]!=' '){
-            j--;
-        }
-        
-        return i-j;
-        
+        // traverse from the end of the string till you get first char...
+
+            while(i>=0 && s[i]==' '){
+                i--;
+            }
+        // traverse till the point where you get another blank space
+
+            int j= i;
+
+            while(j>=0 && s[j]!=' '){
+                j--;
+            }
+
+            // now the difference b/w the first char from last and last char before a blank                 appears is the answer 
+
+            return i-j;
     }
 };
