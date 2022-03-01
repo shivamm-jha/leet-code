8b52757e097b8c54798c0ffbc34a2456db8983a6
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-01 11:39:53 +0530

    Time: 4 ms (88.16%), Space: 7.8 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    int count_bits(int n){
-        int count=0,mask=1;
-        while(n){
-            count+=(mask & n);
-            n = n>>1;
-        }
-        return count;
-    }
+    // int count_bits(int n){
+    //     int count=0,mask=1;
+    //     while(n){
+    //         count+=(mask & n);
+    //         n = n>>1;
+    //     }
+    //     return count;
+    // }
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for(int i=0;i<=n;i++){
-            ans.push_back(count_bits(i));
+        
+//         time complexity 0(n logn)...
+        
+         //vector<int> ans;
+        // for(int i=0;i<=n;i++){
+        //     ans.push_back(count_bits(i));
+        // }
+        // return ans;
+        
+        
+        
+//        time complexity 0(n)
+//         property : if you do i & i-1 it will erase the last set bit of i
+//         for eg. 7 - 111 , 6-110 7&6=110;
+//         so we already know no. of set bits in 0 is 0 
+//         this will hep us to calculate the res. of the rest
+        
+        vector<int>ans(n+1,0);
+        int count=0;
+        for(int i=1;i<=n;i++){
+            ans[i]= 1 + ans[i & (i-1)];
         }
         return ans;
+        
+        
+        
+         
     }
 };
