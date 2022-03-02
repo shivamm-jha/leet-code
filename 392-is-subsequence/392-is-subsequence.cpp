b9961ef28312f0f647264162937f39e5c8e3076e
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-02 10:49:30 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
+    bool is_substr(string &s , string &t , int m , int n){
+        if(m==0)return true;
+        if(n==0) return false;
+        
+        if(s[m-1]==t[n-1]){
+           return  is_substr(s,t,m-1,n-1);
+        }
+        else{
+            return is_substr(s,t,m,n-1);
+        }
+    }
     bool isSubsequence(string s, string t) {
        
-        int k=0,j=0;
+//         int k=0,j=0;
         
-        for(int i=0;i<t.size();i++){
-            if(t[i]==s[j]){
-                k++;
-                j++;
-            }
-        }
-        if(k==s.size())return true;
+//         for(int i=0;i<t.size();i++){
+//             if(t[i]==s[j]){
+//                 k++;
+//                 j++;
+//             }
+//         }
+//         if(k==s.size())return true;
+//         return false;
+        
+        if(is_substr(s,t,s.size(),t.size()))return true;
         return false;
     }
 };
+
+
