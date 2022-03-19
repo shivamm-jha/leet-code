a15403bacc557430af1810f44d72abeca85251ee
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-19 13:15:43 +0530

    Time: 2 ms (58.53%), Space: 6.3 MB (9.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    
+    map<int,int> mp;
     int result(int n){
         if(n==1) return 1;
         if(n==0) return 0;
         
+        if(mp.find(n)!=mp.end()){
+            return mp[n];
+        }
         int last = result(n-1);
         int secondLast = result(n-2);
-        
+        mp[n]= last + secondLast;
         return last + secondLast;
     }
     
