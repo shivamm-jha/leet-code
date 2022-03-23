5b897616a4918a5252f38702568c561c9b123a1e
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-24 01:19:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.88%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
-    int brokenCalc(int startValue, int target) {
-        int count = 0;
-        while(target > startValue){
-            if(target%2==0){
-                target/=2;
-            }
-            else target++;
-            count++;
+    int solve(int startValue , int target){
+        if(startValue >= target){
+            return startValue - target;
         }
+        if(target & 1){
+            return 1 + solve(startValue,target+1);
+        }
+        else{
+            return 1 + solve(startValue, target/2);
+        }
+    }
+    
+    int brokenCalc(int startValue, int target) {
+//         int count = 0;
+//         while(target > startValue){
+//             if(target%2==0){
+//                 target/=2;
+//             }
+//             else target++;
+//             count++;
+//         }
         
        
-        return count + (startValue - target);
+//         return count + (startValue - target);
+        
+       return  solve(startValue, target);
     }
 };
+
+// if(startValue >= target)
+//             return startValue - target;
+        
+//         if(target & 1)// if odd
+//             return 1 + brokenCalc(startValue, target  + 1);
+        
+//         // if even
+//         return 1 + brokenCalc(startValue, target / 2);
