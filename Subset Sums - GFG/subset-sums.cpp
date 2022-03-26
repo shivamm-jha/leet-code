216c5f9019aa05a4465193961a2647ceed33c6a4
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-26 22:21:44 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -6,30 +6,49 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> ans;
-    void solve(vector<int> &nums, vector<int> & empty,int i,int s){
+     vector<int> ans;
+    // void solve(vector<int> &nums, vector<int> & empty,int i){
         
-        // brute force time complexity 0(2^n * n);
+    //     // brute force time complexity 0(2^n * n);
         
-      if(i==nums.size()){
-          
-          ans.push_back(s);
-          return;
+    //   if(i==nums.size()){
+    //       int s=0;
+    //       for(auto &i:empty){
+    //           s+=i;
+    //       }
+    //       ans.push_back(s);
+    //       return;
            
-      }
-        // pick
-       s+=nums[i];
-        solve(nums,empty,i+1,s);
+    //   }
+    //     // pick
+    //     empty.push_back(nums[i]);
+    //     solve(nums,empty,i+1);
+    //     empty.pop_back();
+        
+    //     //don't pick
+    //     solve(nums,empty,i+1);
+        
+    // }
+    
+    //  better solution time complexity 0(2^n)
+    
+    void solve(vector<int>&nums,int i, int s){
+        if(i==nums.size()){
+            ans.push_back(s);
+            return;
+        }
+        
+        s+=nums[i];
+        solve(nums,i+1,s);
         s-=nums[i];
-        
-        //don't pick
-        solve(nums,empty,i+1,s);
-        
+        solve(nums,i+1,s);
     }
     vector<int> subsetSums(vector<int> nums, int n)
     {
-        vector<int> empty;
-        solve(nums,empty,0,0);
+        // vector<int> empty;
+        // solve(nums,empty,0);
+        solve(nums,0,0);
+        
         return ans;
     }
 };
