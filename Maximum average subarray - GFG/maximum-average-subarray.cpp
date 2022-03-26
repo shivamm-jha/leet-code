f822a03c4046c6b6b81f68cfe6fb9851bd9e6b85
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-26 19:42:47 +0530

    Added solution - LeetHub

--- Maximum average subarray - GFG/maximum-average-subarray.cpp
+++ Maximum average subarray - GFG/maximum-average-subarray.cpp
@@ -10,23 +10,46 @@ using namespace std;
 class Solution{   
 public:
     int findMaxAverage(int nums[], int n, int k) {
-       int arr[n];
-       int currSum = 0;
-       for(int i=0;i<n;i++){
-           currSum+=nums[i];
-           arr[i]=currSum;
-       }
+        // time complexity 0(n) .. space complexity 0(n)
+        
+    //   int arr[n];
+    //   int currSum = 0;
+    //   for(int i=0;i<n;i++){
+    //       currSum+=nums[i];
+    //       arr[i]=currSum;
+    //   }
        
-       int max_avg = arr[k-1];
-       int idx=0;
-       for(int i=k;i<n;i++){
-           if(arr[i]-arr[i-k]>max_avg){
-               max_avg=arr[i]-arr[i-k];
-               idx=i-k+1;
-           }
-       }
+    //   int max_avg = arr[k-1];
+    //   int idx=0;
+    //   for(int i=k;i<n;i++){
+    //       if(arr[i]-arr[i-k]>max_avg){
+    //           max_avg=arr[i]-arr[i-k];
+    //           idx=i-k+1;
+    //       }
+    //   }
        
-       return idx;
+    //   return idx;
+    
+    
+    //  reducing the space complexity
+    
+    
+        int currSum =0 , max_sum=0;
+        for(int i=0;i<k;i++){
+            currSum+=nums[i];
+        }
+        int idx = 0;
+        max_sum=currSum;
+        int i=0, j=k;
+        while(j<n){
+            currSum+=nums[j]-nums[i];
+            if(currSum> max_sum){
+                max_sum = currSum;
+                idx=i+1;
+            }
+            i++;j++;
+        }
+        return idx;
     }
 };
 
