65e774f239d0a592dabdbe60f2d891fc265dd486
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-29 22:38:03 +0530

    Time: 120 ms (83.88%), Space: 61.3 MB (26.65%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -11,7 +11,7 @@ public:
        //  }
        //  return 1;
         
-//          better solution  time complexity 0(n) space complexity 0(1)
+//     time complexity 0(n^2) space complexity 0(1)
         
         // for(int i=0;i<nums.size();i++){
         //     for(int j=0;j<nums.size();j++){
@@ -22,12 +22,33 @@ public:
         // }
         // return 1;
         
-        for(int i=0;i<nums.size();i++){
-            int index = abs(nums[i])-1;
-            nums[index]*=-1;
-            if(nums[index]>0) return abs(nums[i]);
+        
+//         if the array is mutable
+        
+        // for(int i=0;i<nums.size();i++){
+        //     int index = abs(nums[i])-1;
+        //     nums[index]*=-1;
+        //     if(nums[index]>0) return abs(nums[i]);
+        // }
+        // return -1;
+        
+        
+//         if the array is mutable
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow= nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        
+        fast = nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        
+        return slow;
+        
     }
 };
  
