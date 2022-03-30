2139092766939c869774d228a6af322f97e69f60
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-03-30 18:23:26 +0530

    Time: 8 ms (35.04%), Space: 9.5 MB (52.00%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int row_start = 0, row_end = n-1;
+        int m = matrix[0].size();
+       
+        while(row_start<=row_end){
+            if(target>=matrix[row_start][0] && target<=matrix[row_start][m-1]){
+                int i=0, j = m-1;
+                while(i<=j){
+                    int mid = (i+j)/2;
+                    if(matrix[row_start][mid]==target){
+                        return true;
+                    }
+                    else if(matrix[row_start][mid]>target){
+                        j=mid-1;
+                    }
+                    else i=mid+1;
+                }
+            }
+           row_start++;
+            
+            
+        }
+    return false;
+    }
+};
