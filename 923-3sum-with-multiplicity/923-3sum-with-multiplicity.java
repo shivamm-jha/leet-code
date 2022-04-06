dd2d8e0d2a89780586507a25eab44368ed58dd0f
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-04-06 20:15:16 +0530

    Time: 755 ms (12.50%), Space: 67 MB (10.89%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,35 +1,35 @@
 class Solution {
-    int mod = 100000007;
+    int mod = 1000000007;
     public  int ans(int[] arr , int target){
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // int count =0;
-        // for(int i=0;i<arr.length;i++){
-        //     for(int j=i+1;j<arr.length;j++){
-        //        int key = target - (arr[i]+arr[j]);
-        //        if(map.containsKey(key)){
-        //            count+=map.get(key);
-        //        }
-        //     }
-        //     count%=mod;
-        //     if(map.containsKey(arr[i])){
-        //         map.put(arr[i],map.get(arr[i])+1);
-        //     }
-        //     else map.put(arr[i],1);
-        // }
-        // return count%mod;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        int res = 0;
-        int mod = 1000000007;
-        for (int i = 0; i < arr.length; i++) {
-            res = (res + map.getOrDefault(target - arr[i], 0)) % mod;
-            
-            for (int j = 0; j < i; j++) {
-                int temp = arr[i] + arr[j];
-                map.put(temp, map.getOrDefault(temp, 0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count =0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+               int key = target - (arr[i]+arr[j]);
+               if(map.containsKey(key)){
+                   count+=map.get(key);
+               }
             }
+            count%=mod;
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else map.put(arr[i],1);
         }
-        return res;
+        return count%mod;
+//         Map<Integer, Integer> map = new HashMap<>();
+        
+//         int res = 0;
+//         int mod = 1000000007;
+//         for (int i = 0; i < arr.length; i++) {
+//             res = (res + map.getOrDefault(target - arr[i], 0)) % mod;
+            
+//             for (int j = 0; j < i; j++) {
+//                 int temp = arr[i] + arr[j];
+//                 map.put(temp, map.getOrDefault(temp, 0) + 1);
+//             }
+//         }
+//         return res;
     }
     public int threeSumMulti(int[] arr, int target) {
         int res = ans(arr,target);
