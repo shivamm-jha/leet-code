c214aa7b3cf212f9906a1e0aa331304ae68843f9
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-04-09 19:05:37 +0530

    Time: 16 ms (81.77%), Space: 10.5 MB (48.18%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-//         o(n^2)
+//        brute : Tc= o(n^2)
         // int count = 0;
         // for(int i=0;i<grid.size();i++){
         //     for(int j=0;j<grid[i].size();j++){
@@ -10,24 +10,38 @@ public:
         // }
         // return count;
         
-//          0(nlog n)
+//        better T.c = 0(n* log m)
         
+        // int count = 0;
+        // for(int i=0;i<grid.size();i++){
+        //     int start = 0 , end = grid[i].size()-1;
+        //     int first_negative=grid[i].size();
+        //     while(start<=end){
+        //         int mid=(start+end)/2;
+        //         if(grid[i][mid]>=0){
+        //             start = mid+1;
+        //         }
+        //         else{
+        //             first_negative=mid;
+        //             end=mid-1;
+        //         }
+        //     }
+        //     count+=grid[i].size()-first_negative;
+        // }
+        // return count;
+        
+//     optimal T.c = 0(n+m)
+        
+        int row = 0 , col = grid[0].size()-1;
         int count = 0;
-        for(int i=0;i<grid.size();i++){
-            int start = 0 , end = grid[i].size()-1;
-            int first_negative=grid[i].size();
-            while(start<=end){
-                int mid=(start+end)/2;
-                if(grid[i][mid]>=0){
-                    start = mid+1;
-                }
-                else{
-                    first_negative=mid;
-                    end=mid-1;
-                }
+        while(row< grid.size() && col>=0){
+            if(grid[row][col]<0){
+                count+= grid.size()-row;
+                col--;
             }
-            count+=grid[i].size()-first_negative;
+            else row++;
         }
         return count;
+        
     }
 };
