6cc08c2afa3df1a7aa93f14aa0862cd42c69aeb8
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-04-13 10:06:33 +0530

    Time: 6 ms (86.42%), Space: 10.6 MB (15.56%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -15,47 +15,48 @@ public:
         
 //         optimal solution 0(n).. sliding window
         
-//         int shortest=INT_MAX;
-//         int n = nums.size();
-//         int left =0 , right =0;
-//         int currSum=0;
-//         while(right<n){
-//             currSum+=nums[right];
-//             if(currSum>=target){
-//                 while(currSum>=target){
-//                     left++;
-//                     currSum-=nums[left];
-//                 }
-//                 shortest= min(shortest, right-left+2);
-//             }
-            
-//             right++;
-//         }
-        
-        
-//         return shortest==INT_MAX? 0 : shortest;
-        
-        int n=nums.size();
-        int sum = 0;            //Current Sum
-        int left = 0;           //left ptr
-        int right = 0;          //right ptr
-        int shortest = INT_MAX; //shortest window with Sum>=Target
-        
-        //Variable Size sliding window: 2-pointer
+        int shortest=INT_MAX;
+        int n = nums.size();
+        int left =0 , right =0;
+        int currSum=0;
         while(right<n){
-            sum += nums[right];//Add current element to Sum
-            
-            if(sum >= target){//Check if currentSum >= Target
-                //Skip all left elements till sum < target (to find the smallest window)
-                while(sum>=target){
-                    sum -= nums[left];
+            currSum+=nums[right];
+            if(currSum>=target){
+                while(currSum>=target){
+                    currSum-=nums[left];
                     left++;
+                    
                 }
-                shortest = min(shortest,right-left+2);//Update smallest window size
+                shortest= min(shortest, right-left+2);
             }
+            
             right++;
         }
-        return shortest==INT_MAX?0:shortest;
+        
+        
+        return shortest==INT_MAX? 0 : shortest;
+        
+//         int n=nums.size();
+//         int sum = 0;            //Current Sum
+//         int left = 0;           //left ptr
+//         int right = 0;          //right ptr
+//         int shortest = INT_MAX; //shortest window with Sum>=Target
+        
+//         //Variable Size sliding window: 2-pointer
+//         while(right<n){
+//             sum += nums[right];//Add current element to Sum
+            
+//             if(sum >= target){//Check if currentSum >= Target
+//                 //Skip all left elements till sum < target (to find the smallest window)
+//                 while(sum>=target){
+//                     sum -= nums[left];
+//                     left++;
+//                 }
+//                 shortest = min(shortest,right-left+2);//Update smallest window size
+//             }
+//             right++;
+//         }
+//         return shortest==INT_MAX?0:shortest;
         
     }
 };
