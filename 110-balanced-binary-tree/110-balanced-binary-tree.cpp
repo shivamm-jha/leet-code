2ae3fd8ad0faf8d9d025f56a41c54b51065aab0c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-02 14:50:40 +0530

    Time: 18 ms (50.44%), Space: 20.9 MB (82.01%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -17,19 +17,33 @@ public:
         }
         
         int leftHeight = height(root->left);
+        if(leftHeight==-1) return -1;
         int rightHeight = height(root->right);
+        if(rightHeight==-1) return -1;
+        if(abs(leftHeight-rightHeight)>1) return -1;
         return max(leftHeight,rightHeight) +1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        if(abs(leftHeight-rightHeight)>1) return false;
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        if(!left || !right) return false;
+//          T.c= 0(N^2) bcoz 0(N) for checking the height and 0(N) for traversal
         
-        return true;
+//         if(root==NULL) return true;
+//         int leftHeight = height(root->left);
+//         int rightHeight = height(root->right);
+//         if(abs(leftHeight-rightHeight)>1) return false;
+//         bool left = isBalanced(root->left);
+//         bool right = isBalanced(root->right);
+//         if(!left || !right) return false;
+        
+//         return true;
+        
+        
+//          we can reduce it to 0(N) 
+//         The approach is a slight modification in the height function
+//         after finding the height of the left and right subtree 
+//          we will check if their absolute difference is less than equal to 1
+//          if not then there itself we will return -1
+        
+        return height(root)!=-1;
         
     }
 };
