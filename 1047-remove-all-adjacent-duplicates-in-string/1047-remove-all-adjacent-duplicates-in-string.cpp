15e2c16b4bc15032dda149c709fec5950c8e4010
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-06 11:36:18 +0530

    Time: 35 ms (42.08%), Space: 11 MB (70.71%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char> st;
-        for(auto &it:s){
-           if(!st.empty() && st.top()==it){
-               st.pop();
-           }
-            else st.push(it);
-        }
+//         T.c = 0(N) , S.C = 0(N)
         
-        string str="";
-        while(!st.empty()){
-            str+=st.top();
-            st.pop();
+//         stack<char> st;
+//         for(auto &it:s){
+//            if(!st.empty() && st.top()==it){
+//                st.pop();
+//            }
+//             else st.push(it);
+//         }
+        
+//         string str="";
+//         while(!st.empty()){
+//             str+=st.top();
+//             st.pop();
+//         }
+//         reverse(str.begin(),str.end());
+//         return str;
+        
+//         recurrsive solution gives memeory limit exceeded ..
+        
+        // for(int i=1;i<s.size();i++){
+        //     if(s[i]==s[i-1]){
+        //         return removeDuplicates(s.substr(0,i-1)+s.substr(i+1,s.size()));
+        //     }
+        // }
+        // return s;
+        
+        
+//         best solution .. two pointers , T.C = 0(N) , S.C = 0(1)
+        
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(ans.size()==0)ans.push_back(s[i]);
+            else if(ans.back()==s[i]){
+                ans.pop_back();
+            }
+            else ans.push_back(s[i]);
         }
-        reverse(str.begin(),str.end());
-        return str;
+        return ans;
+        
+
+        
     }
 };
