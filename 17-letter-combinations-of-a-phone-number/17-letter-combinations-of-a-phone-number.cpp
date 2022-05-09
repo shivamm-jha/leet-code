2fe772e884bb130bb1bba193c3fba93a4e113ff2
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-09 19:46:29 +0530

    Time: 5 ms (16.72%), Space: 6.7 MB (35.85%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,24 @@
 class Solution {
-private:
-    map<int, string> m;
-    vector<string> ans;
 public:
-    vector<string> letterCombinations(string digits) {
-        if(digits=="") return ans;
-        m[2] = "abc";
-        m[3] = "def";
-        m[4] = "ghi";
-        m[5] = "jkl";
-        m[6] = "mno";
-        m[7] = "pqrs";
-        m[8] = "tuv";
-        m[9] = "wxyz";
-        
-        string currString="";
-        makeCombinations(digits, currString, 0);
-        
-        return ans;
-            
-    }
-    
-    
-   void makeCombinations(string digits, string &currString, int i){
-        if(i==digits.size()){
-            ans.push_back(currString);
+    vector<string> ans;
+    vector<string> mp = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void solve(string digits, string output , int index){
+        if(index>= digits.size()){
+            ans.push_back(output);
             return;
         }
         
-        for(auto letter : m[digits[i]-'0']){
-            currString.push_back(letter);
-            makeCombinations(digits, currString, i+1);
-            currString.pop_back();
+        string temp = mp[digits[index]-'0'];
+        for(int i=0;i<temp.size();i++){
+            output.push_back(temp[i]);
+            solve(digits,output,index+1);
+            output.pop_back();
         }
-        return; 
-    }  
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return ans;
+        string output="";
+        solve(digits,output,0);
+        return ans;
+    }
 };
