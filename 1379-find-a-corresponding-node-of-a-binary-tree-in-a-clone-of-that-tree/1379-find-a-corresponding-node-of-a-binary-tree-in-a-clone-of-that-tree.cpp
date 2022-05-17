b07c0c2792fee8edfceb7256541af5f300c88e2c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-17 22:00:21 +0530

    Time: 821 ms (28.77%), Space: 165 MB (9.19%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -15,32 +15,42 @@ public:
         q.push(cloned);
         while(!q.empty()){
             int n= q.size();
-            TreeNode* node=q.front();
-            if(node->val==target->val){
-                return node;
-            }
             for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
                 if(node->left)q.push(node->left);
                 if(node->right)q.push(node->right);
+                if(node->val==target->val){
+                    return node;
+                }
             }
-            q.pop();
+            
         }
         
         return NULL;
     }
-    TreeNode * ans= NULL;
-    void inorder(TreeNode * cloned, TreeNode * target){
-        if(cloned!=NULL){
-            inorder(cloned->left,target);
-            if(cloned->val==target->val){
-                ans=cloned; return;
-            }
-            inorder(cloned->right,target);
-        }
-    }
+    
+//      recurrsive inorder approach ...
+    
+    
+    // TreeNode * ans= NULL;
+    // void inorder(TreeNode * cloned, TreeNode * target){
+    //     if(cloned!=NULL){
+    //         inorder(cloned->left,target);
+    //         if(cloned->val==target->val){
+    //             ans=cloned; return;
+    //         }
+    //         inorder(cloned->right,target);
+    //     }
+    // }
+    
+    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        inorder(cloned,target);
-        return ans;
+        // inorder(cloned,target);
+        
+        // return ans;
+        
+        return bfs(cloned,target);
         
     }
 };
