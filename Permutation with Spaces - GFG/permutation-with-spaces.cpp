429b2827fd8702ce5587550386ea022551020252
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-18 01:54:46 +0530

    Added solution - LeetHub

---
+++ Permutation with Spaces - GFG/permutation-with-spaces.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+public:
+    vector<string> ans;
+    void dfs(string &empty, string &s , int i ){
+        if(i+1>=s.size()){
+            empty+=s[i];
+            ans.push_back(empty);
+            empty.pop_back();
+            return;
+        }
+        empty+=s[i];
+        empty+=" ";
+        dfs(empty,s,i+1);
+        empty.pop_back();
+        dfs(empty,s,i+1);
+        empty.pop_back();
+    }
+    vector<string> permutation(string S){
+        string empty;
+        dfs(empty,S,0);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int  main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        vector<string> ans;
+        Solution obj;
+        ans = obj.permutation(S);
+        for(int i=0;i<ans.size();i++){
+            cout<<"("<<ans[i]<<")";
+        }
+        cout << endl;
+    }
+}
+  // } Driver Code Ends
