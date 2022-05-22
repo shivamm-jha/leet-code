0a42b9adb2ba5a5cefc8bd52c3cd4d867b10ab86
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-22 17:45:34 +0530

    Time: 234 ms (12.54%), Space: 6.2 MB (96.68%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
+    // int dp[]
     int countSubstrings(string s) {
-        int n = s.size();
-        int count =0;
-        for(int i=0;i<n;i++){
-            string str="";
-            for(int j=i;j<n;j++){
-                str+=s[j];
-                if(isPalindrome(str))count++;
+        int count = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            for(int j = i; j < s.size(); ++j) {
+                count += helper(s, i, j);
             }
         }
         return count;
     }
     
-     bool isPalindrome(string s){
-        int i=0, j=s.size()-1;
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+    
+//     function to check a string is palindrome or not (recurrsive)
+    
+    int helper(string& s, int i, int j) {
+        if (i >= j) return 1;
+        return s[i] == s[j] ? helper(s, i+1, j-1) : 0;
     }
+    
+    
+    
+//      funtion to check a string is palindrome or not (iterative)
+    
+    //  bool isPalindrome(string s){
+    //     int i=0, j=s.size()-1;
+    //     while(i<j){
+    //         if(s[i]!=s[j]){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
     
 };
