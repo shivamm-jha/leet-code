edc67788851435e99bf969e30c648f3fd802f31b
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-24 22:07:04 +0530

    Time: 19 ms (56.36%), Space: 13.3 MB (18.94%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,42 +1,58 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> v;
+        // vector<pair<int,int>> v;
         int n = matrix.size();
         int m = matrix[0].size();
+        unordered_set<int> row;
+        unordered_set<int> col;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    v.push_back({i,j});
+                    // v.push_back({i,j});
+                    row.insert(i);
+                    col.insert(j);
                 }
             }
         }
         
-        
-        for(auto &it: v){
-            int x =it.first;
-            int y = it.second;
-            int i=x, j=y;
-            while(i>=0){
-                matrix[i][j]=0;
-                i--;
-            }
-            i=x;
-            while(i<n){
-                matrix[i][j]=0;
-                i++;
-            }
-            
-            i=x,j=y;
-            while(j>=0){
-                matrix[i][j]=0;
-                j--;
-            }
-            j=y;
-            while(j<m){
-                matrix[i][j]=0;
-                j++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(row.find(i)!=row.end() || col.find(j)!=col.end()){
+                    matrix[i][j]=0;
+                }
             }
         }
+        
+//         T.c = m*n(2n + 2m) in worst case
+        
+//         for(auto &it: v){
+//             int x =it.first;
+//             int y = it.second;
+//             int i=x, j=y;
+//             while(i>=0){
+//                 matrix[i][j]=0;
+//                 i--;
+//             }
+//             i=x;
+//             while(i<n){
+//                 matrix[i][j]=0;
+//                 i++;
+//             }
+            
+//             i=x,j=y;
+//             while(j>=0){
+//                 matrix[i][j]=0;
+//                 j--;
+//             }
+//             j=y;
+//             while(j<m){
+//                 matrix[i][j]=0;
+//                 j++;
+//             }
+//         }
+        
+        
+        
     }
 };
