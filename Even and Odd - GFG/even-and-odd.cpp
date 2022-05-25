6d6e2682c5f1df0e1e62364df4e9d29722ebaabe
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-26 00:48:39 +0530

    Added solution - LeetHub

--- Even and Odd - GFG/even-and-odd.cpp
+++ Even and Odd - GFG/even-and-odd.cpp
@@ -9,21 +9,49 @@ using namespace std;
 class Solution {
   public:
     void reArrange(int arr[], int n) {
-        vector<int> even;
-        vector<int> odd;
+        // vector<int> even;
+        // vector<int> odd;
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]%2==0){
+        //         even.push_back(arr[i]);
+        //     }
+        //     else odd.push_back(arr[i]);
+        // }
+        // int j=0, k=0;
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0){
+        //         arr[i]=even[j++];
+        //     }
+        //     else arr[i]=odd[k++];
+        // }
+        
+        int first_odd =-1, first_even=-1;
         for(int i=0;i<n;i++){
             if(arr[i]%2==0){
-                even.push_back(arr[i]);
+                first_even=arr[i]; break;
             }
-            else odd.push_back(arr[i]);
         }
-        int j=0, k=0;
+        
         for(int i=0;i<n;i++){
-            if(i%2==0){
-                arr[i]=even[j++];
+            if(arr[i]%2!=0){
+                first_odd=arr[i]; break;
             }
-            else arr[i]=odd[k++];
         }
+        
+        for(int i=0;i<n;i++){
+            //cout<<first_odd<<" "<<first_even<<endl;
+            if(i%2==0 && arr[i]%2!=0){
+                first_odd=arr[i];
+                arr[i]=first_even;
+            }
+            else if(i%2!=0 && arr[i]%2==0){
+                first_even =arr[i];
+                arr[i]=first_odd;
+            }
+            else continue;
+        }
+        
+        //for(int i=0;i<n;i++)cout<<arr[i]<<" ";
     }
 };
 
