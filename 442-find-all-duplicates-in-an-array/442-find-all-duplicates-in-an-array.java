b67a058ff2a54cf58b37c8942544ce58f0082d4d
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-05-29 23:42:05 +0530

    Time: 7 ms (78.35%), Space: 64 MB (68.91%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-//         T.c = 0(N^2)
+//         T.c = 0(N^2) , S.C = 0(1)
         // List<Integer> ans = new ArrayList<Integer>();
         // int n = nums.length;
         // for(int i=0;i<n-1;i++){
@@ -12,20 +12,39 @@ class Solution {
         // }
         // return ans;
         
-//         better solution
+//         better solution .... T.C = 0(N) , S.C = 0(1)
         
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
+//         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+//         for(int num : nums){
+//             map.put(num, map.getOrDefault(num,0)+1);
+//         }
         
+//         List<Integer> ans = new ArrayList<Integer>();
+//         for(int num : nums){
+//             if(map.get(num)==2){
+//                 ans.add(num);
+//                 map.put(num,0);
+//             }
+//         }
+//         return ans;
+        
+        
+        
+//         best solution T.C = 0(N), S.C = 0(1)
         List<Integer> ans = new ArrayList<Integer>();
-        for(int num : nums){
-            if(map.get(num)==2){
-                ans.add(num);
-                map.put(num,0);
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int idx = Math.abs(nums[i])-1;
+            if(nums[idx]<0){
+                ans.add(idx+1);
+            }
+            else{
+                nums[idx]= - nums[idx];
+                
             }
         }
+        
         return ans;
+        
     }
 }
