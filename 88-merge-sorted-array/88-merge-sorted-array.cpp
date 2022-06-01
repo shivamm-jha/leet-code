bdefb36c3f061f80b8a65d68d8bd1590dac78b9c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-02 02:28:03 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (72.77%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -8,27 +8,47 @@ public:
         
 //         sort(nums1.begin(),nums1.end());
         
-        vector<int> dummy;
-        int i =0 , j =0;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                dummy.push_back(nums1[i]);
-                i++;
-            }
-            else dummy.push_back(nums2[j++]);
+        
+        
+//          T.C = 0(N+ M) , S.C = 0(N+M)
+        
+//         vector<int> dummy;
+//         int i =0 , j =0;
+//         while(i<m && j<n){
+//             if(nums1[i]<=nums2[j]){
+//                 dummy.push_back(nums1[i]);
+//                 i++;
+//             }
+//             else dummy.push_back(nums2[j++]);
             
+//         }
+        
+//         while(i<m){
+//             dummy.push_back(nums1[i++]);
+//         }
+//         while(j<n){
+//             dummy.push_back(nums2[j++]);
+//         }
+        
+//         for(int i=0;i<dummy.size();i++){
+//             nums1[i]=dummy[i];
+//         }
+        
+        
+        int idx1 = m-1;
+        int idx2 = n-1;
+        int i = n+m-1;
+        while(idx1>=0 && idx2>=0){
+            if(nums1[idx1] > nums2[idx2]){
+                nums1[i--]=nums1[idx1--];
+            }
+            else nums1[i--]=nums2[idx2--];
         }
         
-        while(i<m){
-            dummy.push_back(nums1[i++]);
+        while(idx2>=0){
+            nums1[i--]=nums2[idx2--];
         }
-        while(j<n){
-            dummy.push_back(nums2[j++]);
-        }
-        
-        for(int i=0;i<dummy.size();i++){
-            nums1[i]=dummy[i];
-        }
+       
         
     }
 };
