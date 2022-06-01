4571b67f3777b2aadd0dd3625dca6020fc177bef
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-02 01:28:33 +0530

    Time: 71 ms (83.27%), Space: 48.4 MB (78.00%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int dp[10001];
+//      recurrsion + memoization...
+//     T.C = 0(N) , S.C = 0(N)
     bool solve(vector<int>&nums, int i){
         if(i==nums.size()-1)return true;
         if(nums[i]==0) return false;
@@ -13,7 +15,19 @@ public:
         return dp[i]=false;
     }
     bool canJump(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(nums,0);
+        
+        
+        int reachable =0;
+        for(int i=0;i<nums.size();i++){
+            if(i> reachable) return false;
+           
+            if(nums[i]!=0)reachable = max(reachable, i+ nums[i]);
+            
+        }
+        
+        return reachable >= nums.size()-1 ? true : false;
     }
 };
+
