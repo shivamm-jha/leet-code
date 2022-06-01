fb10338e854329200201cd2a005919cee11e1002
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-02 01:00:32 +0530

    Time: 1669 ms (5.00%), Space: 49.3 MB (14.78%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[10001];
+    bool solve(vector<int>&nums, int i){
+        if(i==nums.size()-1)return true;
+        if(nums[i]==0) return false;
+        if(dp[i]!=-1) return dp[i];
+        for(int j=1;j<=nums[i];j++){
+            if(solve(nums,i+j)){
+                return dp[i]=true;
+            }
+        }
+        return dp[i]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0);
+    }
+};
