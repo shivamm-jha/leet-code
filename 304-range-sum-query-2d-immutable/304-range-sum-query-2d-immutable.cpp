5ebd3c3841cbe9e7c303c9035c3565fccf4d6edc
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-03 12:32:23 +0530

    Time: 557 ms (61.30%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,66 +1,27 @@
 class NumMatrix {
-    int [][] row;
-    // int [][] col;
-    public NumMatrix(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        row = new  int [n][m];
-        // col = new  int [n][m];
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                row[i][j]=matrix[i][j];
+public:
+    vector<vector<int>> nums;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        nums = vector<vector<int>>(n+1, vector<int>(m+1));
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=m;++j){
+                nums[i][j] = nums[i-1][j] + nums[i][j-1] 
+                    - nums[i-1][j-1] + matrix[i-1][j-1] ;
             }
         }
-        // for(int i=0;i<n;++i){
-        //     for(int j=0;j<m;++j){
-        //         col[i][j]=matrix[i][j];
-        //     }
-        // }
-        int rowSum =0;
-        for(int i = 0;i<n;++i){
-            rowSum=0;
-            for(int j=0;j<m;++j){
-                rowSum+=row[i][j];
-                row[i][j]=rowSum;
-            }
-        }
-    
-        // int colSum=0;
-        // for(int i=0;i<n;++i){
-        //     colSum=0;
-        //     for(int j=0;j<m;++j){
-        //         colSum+= col[j][i];
-        //         col[j][i]=colSum;
-        //     }
-        // }
-        
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                System.out.print(row[i][j]+" ");
-            }
-            System.out.print("\n");
-        }
-        System.out.print("\n");
-        // for(int i=0;i<n;++i){
-        //     for(int j=0;j<m;++j){
-        //         System.out.print(col[i][j]+" ");
-        //     }
-        //     System.out.print("\n");
-        // }
     }
     
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int res =0;
-        for(int i=row1;i<=row2;i++){
-            int temp = col1 == 0? 0 : row[i][col1-1];
-            res+= (row[i][col2] - temp);
-        }
-        return res;
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int total = nums[row2+1][col2+1];
+        int extra = nums[row1][col2+1] + nums[row2+1][col1] - nums[row1][col1];
+        return total - extra;
     }
-}
+};
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
  */
