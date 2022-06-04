fefe3aa056dbd3512d081903ba010abe20fb00ba
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-04 22:35:38 +0530

    Time: 23 ms (36.47%), Space: 13.4 MB (19.47%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,30 +1,70 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+    unordered_map<int,int>rows,cols;
+    int n = matrix.size();
+    int m = matrix[0].size();
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            if(matrix[i][j]==0){
+                rows[i]++;
+                cols[j]++;
+            }
+        }
+    }
+    
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            if(rows.find(i)!=rows.end() || cols.find(j)!=cols.end()){
+                matrix[i][j]=0;
+            }
+        }
+    }
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+// //          best solution T.C = 0(M*N)  , S.C =0(1);
+        
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(matrix[i][j]==0){
+//                     matrix[0][j]=0;
+//                     matrix[i][0]=0;
+//                 }
+//             }
+//         }
+        
+        
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(matrix[i][0]==0 || matrix[0][j]==0){
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
         // vector<pair<int,int>> v;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        unordered_set<int> row;
-        unordered_set<int> col;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    // v.push_back({i,j});
-                    row.insert(i);
-                    col.insert(j);
-                }
-            }
-        }
+        // unordered_set<int> row;
+        // unordered_set<int> col;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(matrix[i][j]==0){
+        //             // v.push_back({i,j});
+        //             row.insert(i);
+        //             col.insert(j);
+        //         }
+        //     }
+        // }
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row.find(i)!=row.end() || col.find(j)!=col.end()){
-                    matrix[i][j]=0;
-                }
-            }
-        }
+//         This approach is better T.c = 0(M*N) , S.C = 0(M+N)
         
-//         T.c = m*n(2n + 2m) in worst case
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(row.find(i)!=row.end() || col.find(j)!=col.end()){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+        
+//         Brute Force sol.  T.c = m*n(2n + 2m) in worst case
         
 //         for(auto &it: v){
 //             int x =it.first;
