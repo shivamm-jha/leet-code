edc2d0c1d030c6a9476687770bdf4c03adcb2124
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-10 19:05:28 +0530

    Time: 32 ms (42.35%), Space: 10.8 MB (26.11%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int ans =0;
+        // int n = s.size();
+        // int ans =0;
 //          T.C = 0(N^2)  , S.C = 0(ans)
         // for(int i=0;i<n;++i){
         //     unordered_set<char> st;
@@ -17,21 +17,38 @@ public:
         // }
         // return ans;
         
-        unordered_map<char,int> hash;
-        int start =0;
-        for(int idx=0;idx<n;++idx){
-            if(hash.find(s[idx])==hash.end()){
-                hash[s[idx]]=idx;
-                ans = max(ans, idx-start +1);
+        
+//          T.C = 0(N) , S.C = 0(ANS)
+        
+//         unordered_map<char,int> hash;
+//         int start =0;
+//         for(int idx=0;idx<n;++idx){
+//             if(hash.find(s[idx])==hash.end()){
+//                 hash[s[idx]]=idx;
+//                 ans = max(ans, idx-start +1);
                 
+//             }
+//             else{
+//                 auto it = hash.find(s[idx]);
+//                 int temp = it->second +1;
+//                 start = max(start,temp);
+//                 hash.erase(it);
+//                 hash[s[idx]]=idx;
+//                 ans= max(ans, idx - start +1);
+//             }
+//         }
+//         return ans;
+        
+        
+        unordered_set<char> st;
+        int i =0 , n= s.size() , j=0 , ans =0;
+        while(i<n && j<n){
+            if(st.find(s[j])==st.end()){
+                st.insert(s[j++]);
+                ans = max(ans,j-i);
             }
             else{
-                auto it = hash.find(s[idx]);
-                int temp = it->second +1;
-                start = max(start,temp);
-                hash.erase(it);
-                hash[s[idx]]=idx;
-                ans= max(ans, idx - start +1);
+                st.erase(s[i++]);
             }
         }
         return ans;
