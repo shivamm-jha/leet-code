187857f2c1164948a092757bba2303b4cf81b951
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-16 19:16:55 +0530

    Time: 20 ms (5.68%), Space: 13.1 MB (7.70%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-    vector<int> minlen(triangle.back());
-    for (int layer = n-2; layer >= 0; layer--) // For each layer
-    {
-        for (int i = 0; i <= layer; i++) // Check its every 'node'
-        {
-            // Find the lesser of its two children, and sum the current value in the triangle with it.
-            minlen[i] = min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
-        }
+    int dp[1000][1000];
+    int solve(vector<vector<int>> &nums , int i , int j){
+        if(i==nums.size()) return 0;
+        if(j==nums[i].size())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int move1 = nums[i][j] + solve(nums,i+1,j);
+        int move2 = nums[i][j] + solve(nums,i+1,j+1);
+        return dp[i][j] = min(move1,move2);
     }
-    return minlen[0];
+    int minimumTotal(vector<vector<int>>& triangle) {
+        memset(dp,-1,sizeof(dp));
+        return solve(triangle,0,0);
     }
 };
