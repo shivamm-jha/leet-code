26deec5cb1a3ea54f53ae5264baffff1e7626bfe
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-26 10:52:30 +0530

    Time: 237 ms (5.10%), Space: 42.4 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    
-//      recurrsive code....
-//     memoization will also give tle bcoz in worst case it can go upto N^2
-  
-    int solve(vector<int>&nums,int i , int j, int k){
-       if(k==0) return 0 ;
-        int profit =0;
-        if(i<=j){
-           int front = nums[i] + solve(nums,i+1,j,k-1);
-           int back = nums[j] + solve(nums,i,j-1,k-1);
-           profit = max(front,back);
-        }
-        return profit;
-    }
     int maxScore(vector<int>& nums, int k) {
-//         brute force gives tle...
-        
-        // int ans = solve(nums,0,nums.size()-1,k);
-        // return ans;
-        
-        
-//         approach is to find a subarray of length n-k which has minimum sum
-//          and then subtract it from the total sum of array
-        int n = nums.size();
-        int totalSum =0;
-        for(auto &it:nums){
-            totalSum+=it;
-        }
-        if(n==k) return totalSum;
-        
-        int window = 0;
-        for(int i=0;i<n-k-1;i++){
-            window+=nums[i];
+        int total = 0;
+        for(auto &it : nums){
+            total+=it;
         }
         
-        int ans=0;
+        int subarraySum = 0;
+        int n =  nums.size();
+        int i=0;
+        for(;i<n-k;i++){
+            subarraySum+=nums[i];
+        }
+        // cout<<subarraySum<<endl;
+        int val = subarraySum;
         int j=0;
-        for(int i=n-k-1;i<n;i++){
-            window += nums[i];
-            ans = max(ans, totalSum - window);
-            window -= nums[j++];
+        while(i<n){
+            cout<<subarraySum<<endl;
+            subarraySum+=(nums[i++]-nums[j++]);
+            val= min(val,subarraySum);
+            
         }
         
-        return ans;
+        
+        return total - val;
+        
     }
 };
