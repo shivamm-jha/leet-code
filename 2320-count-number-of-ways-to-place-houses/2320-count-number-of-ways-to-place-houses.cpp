f94048d53cd6fabeeea9e65f7a837f74eafd1ba8
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-06-26 12:08:20 +0530

    Time: 28 ms (75.00%), Space: 7.1 MB (50.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    int mod = 1e9+7;
-    int sol(int n, vector<int>&dp)
-    {
-        if(n < 0) return 1;
+    #define ll long long
+    int mod = 1e9 + 7;;
+    ll dp[100000];
+    ll solve(int n){
+        if(n==0) return 1;
+        if(n==1) return 2;
+        if(dp[n]!=-1)return dp[n];
+        ll pick = solve(n-2);
+        ll npick = solve(n-1);
+        return dp[n]=(pick+npick)%mod;
         
-        if(dp[n] != -1) return dp[n];
-        
-        int place = sol(n-2,dp);
-        int not_place = sol(n-1,dp);
-        
-        return dp[n] = (place + not_place) % mod;
+    }
+    int countHousePlacements(int n) {
+        memset(dp,-1,sizeof(dp));
+        ll ans = solve(n);
+        return ans*ans %mod;
     }
     
-    int countHousePlacements(int n) {
-        vector<int>dp(n,-1);
-        sol(n-1,dp);
-        return (1LL * dp[n-1] * dp[n-1]) % mod;
-    }
+
 };
+
+
+// int countHousePlacements(int n) {
+//         vector<long long> dp(n + 10);
+//         int mod = 1e9 + 7;
+//         dp[0] = 1, dp[1] = 2;
+//         for (int i = 2; i <= n; ++i)
+//             dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
+//         return dp[n] * dp[n] % mod;
+//     }
+
+
+
