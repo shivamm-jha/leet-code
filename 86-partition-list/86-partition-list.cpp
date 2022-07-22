226c6112cb4d35b17289b9fa6af1db27e6348888
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-07-22 23:20:00 +0530

    Time: 11 ms (39.33%), Space: 10.3 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,25 +11,48 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode * dummy = new ListNode(0);
+//         ListNode * dummy = new ListNode(0);
+//         ListNode * curr = head;
+//         ListNode* temp = dummy;
+//         while(curr!=NULL){
+//             if(curr->val < x){
+//                 temp->next = new ListNode(curr->val);
+//                 temp=temp->next;
+//             }
+//             curr=curr->next;
+//         }
+        
+//         curr = head;
+//         while(curr!=NULL){
+//             if(curr->val >= x){
+//                 temp->next=new ListNode(curr->val);
+//                 temp = temp->next;
+//             }
+//             curr =  curr->next;
+//         }
+//         return dummy->next;
+        
+//          one pass solution...
+        ListNode * left= new ListNode(0);
+        ListNode * dummy1= left;
+        ListNode * right = new ListNode(0);
+        ListNode * dummy2=right;
         ListNode * curr = head;
-        ListNode* temp = dummy;
         while(curr!=NULL){
             if(curr->val < x){
-                temp->next = new ListNode(curr->val);
-                temp=temp->next;
+                left->next = curr;
+                left = left->next;
             }
-            curr=curr->next;
+            else{
+                right->next=curr;
+                right = right->next;
+            }
+             curr=curr->next;
         }
         
-        curr = head;
-        while(curr!=NULL){
-            if(curr->val >= x){
-                temp->next=new ListNode(curr->val);
-                temp = temp->next;
-            }
-            curr =  curr->next;
-        }
-        return dummy->next;
+        left->next = dummy2->next;
+        right->next = NULL;
+        return dummy1->next;
+        
     }
 };
