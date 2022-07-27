d656a26439185de5a0983e004afc9ac2898c7a40
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-07-27 20:01:28 +0530

    Time: 9 ms (61.47%), Space: 12.6 MB (78.06%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -18,21 +18,36 @@ public:
             preorder(root->right,v);
         }
     }
+    TreeNode * prev= NULL;
     void flatten(TreeNode* root) {
-        vector<TreeNode*> v;
-        preorder(root,v);
-        TreeNode * curr=root;
-        for(auto &it: v)cout<<it->val<<" ";
-        cout<<endl;
-        int i =1;
-        while(i<v.size()){
-            TreeNode * newNode = new TreeNode(v[i]->val);
-            curr->left=NULL;
-            curr->right=newNode;
-            curr=newNode;
-            i++;
-            
-        }
+//          very bad naive solution
         
+//         vector<TreeNode*> v;
+//         preorder(root,v);
+//         TreeNode * curr=root;
+//         for(auto &it: v)cout<<it->val<<" ";
+//         cout<<endl;
+//         int i =1;
+//         while(i<v.size()){
+//             TreeNode * newNode = new TreeNode(v[i]->val);
+//             curr->left=NULL;
+//             curr->right=newNode;
+//             curr=newNode;
+//             i++;
+            
+//         }
+        
+        
+//          reverse postorder 
+//          visit right -> left -> root
+//         change the right link of the right node to prev & left link to null and call recurrsion again
+        
+        if(root==NULL)return;
+        
+        flatten(root->right);
+        flatten(root->left);
+        root->left=NULL;
+        root->right=prev;
+        prev=root;
     }
 };
