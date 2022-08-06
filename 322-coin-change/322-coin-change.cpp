b6b11ca96e522c4aa529cd263878d0a0df1eec0b
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-08-06 19:34:16 +0530

    Time: 82 ms (90.28%), Space: 11.3 MB (78.49%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int dp[10010];
-    int minCoins(vector<int>&coins, int amount){
-        if(amount==0) return 0;
-        if(dp[amount]!=-1)return dp[amount];
-        int ans = INT_MAX-1;
-        for(int coin : coins){
-            if(amount-coin>=0)
-                ans= min(ans, minCoins(coins,amount-coin)+1);
+    int dp[10001][20];
+    int solve(vector<int>&coins, int amount, int i){
+        if(i==coins.size()-1){
+            if(amount%coins[i]==0)return amount/coins[i];
+            return 1e9;
         }
-        return dp[amount]=ans;
+        if(dp[amount][i]!=-1)return dp[amount][i];
+        int notTake = 0+ solve(coins,amount,i+1);
+        int take = INT_MAX;
+        if(amount>=coins[i]){
+             take = 1+solve(coins,amount-coins[i],i);
+        }
+        return dp[amount][i]=min(take,notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
         memset(dp,-1,sizeof(dp));
-        int ans = minCoins(coins,amount);
-        return ans==INT_MAX-1? -1 : ans;
+        int ans = solve(coins,amount,0);
+        return ans>=1e9 ? -1 : ans;
     }
 };
