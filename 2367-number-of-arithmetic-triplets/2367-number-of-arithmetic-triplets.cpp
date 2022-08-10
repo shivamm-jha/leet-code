dd63ff02ac42489456985b95c8770fd8e9dfab8b
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-08-10 21:19:14 +0530

    Time: 3 ms (100.00%), Space: 8.5 MB (25.00%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -2,23 +2,55 @@ class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         int n = nums.size();
-        int cnt =0;
+//         int cnt =0;
+//         for(int i=0;i<n-2;i++){
+//             int j=i+1;
+//             while(j<n-1){
+//                 if(nums[j]-nums[i]==diff)break;
+//                 j++;
+//             }
+//             int k = j+1;
+//             while(k<n){
+//                 if(nums[k]-nums[j]==diff){
+//                     cnt++;
+//                     break;
+//                 }
+//                 k++;
+//             }
+//         }
+        
+//         return cnt;
+        int cnt=0;
         for(int i=0;i<n-2;i++){
-            int j=i+1;
-            while(j<n-1){
-                if(nums[j]-nums[i]==diff)break;
-                j++;
-            }
-            int k = j+1;
-            while(k<n){
-                if(nums[k]-nums[j]==diff){
-                    cnt++;
-                    break;
-                }
-                k++;
+            int j = diff+nums[i];
+            int j_idx= search(nums,i+1,n-2,j);
+            // cout<<j<<" "<<j_idx<<endl;
+            if(j_idx!=-1){
+                int k = diff+nums[j_idx];
+                int k_idx= search(nums,j_idx+1,n-1,k);
+                // cout<<k<<endl;
+                if(k_idx!=-1)cnt++;
             }
         }
         
+        // int ans= search(nums,0,n-2,4);
+        // cout<<ans<<endl;
         return cnt;
+    }
+    
+    int search(vector<int>&nums,int l, int h, int key){
+        int low =l, high=h;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]==key){
+                return mid;
+            }
+            else if(nums[mid]<key){
+                low=mid+1;
+            }
+            else high =mid-1;
+        }
+       return -1;
     }
 };
