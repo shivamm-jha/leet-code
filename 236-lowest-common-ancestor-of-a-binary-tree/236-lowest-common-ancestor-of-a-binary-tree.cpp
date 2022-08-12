7f85b059d8c1accbf05b435c9fb04eeef9dccc5c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-08-12 16:19:03 +0530

    Time: 37 ms (14.07%), Space: 17.3 MB (8.48%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -18,7 +18,34 @@ public:
         if(right==NULL)return left;
         return root;
     }
+    
+//      function to find path from root to gven node..
+    
+    bool findPath(TreeNode * root,vector<TreeNode*>&path,TreeNode* key){
+        if(root==NULL)return false;
+        path.push_back(root);
+        if(root==key){
+            return true;
+        }
+        bool leftSubtree = findPath(root->left,path,key);
+        if(leftSubtree)return true;
+        bool rightSubtree = findPath(root->right,path,key);
+        if(rightSubtree)return true;
+        path.pop_back();
+        return false;
+        
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return solve(root, p, q);
+        // return solve(root, p, q);
+        
+        vector<TreeNode*> path1,path2;
+        findPath(root,path1,p);
+        findPath(root,path2,q);
+        int i=0;
+        while(i<path1.size() && i<path2.size()){
+            if(path1[i]==path2[i])i++;
+            else break;
+        }
+        return path1[i-1];
     }
 };
