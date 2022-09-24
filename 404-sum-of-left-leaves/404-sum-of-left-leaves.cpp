9bf6ce0f645d324750921e3fdb7b8d4a043acb93
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-09-24 19:50:10 +0530

    Time: 3 ms (88.04%), Space: 13.5 MB (43.42%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -1,47 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        if(root==NULL && (root->left==NULL && root->right==NULL))return 0;
+        if(root==NULL) return 0;
+        
+        int sum = 0;
+        
         queue<TreeNode*> q;
-        int sum =0;
         q.push(root);
-        
-        
-        
         while(!q.empty()){
-            TreeNode * node= q.front();
+            auto front = q.front();
             q.pop();
-            if(node->left && node->left->left==NULL && node->left->right==NULL)sum+=node->left->val;
-            if(node->left)q.push(node->left);
-            if(node->right)q.push(node->right);
             
+            if(front->left && front->left->left==NULL && front->left->right==NULL){
+                sum+= front->left->val;
+            }
+            
+            if(front->left)q.push(front->left);
+            
+            if(front->right)q.push(front->right);
         }
+        
         return sum;
     }
 };
-
-// f(root == null || root.left == null && root.right == null) return 0;
-        
-//         int res = 0;
-//         Queue<TreeNode> queue = new LinkedList<>();
-//         queue.offer(root);
-        
-//         while(!queue.isEmpty()) {
-//             TreeNode curr = queue.poll();
-
-//             if(curr.left != null && curr.left.left == null && curr.left.right == null) res += curr.left.val;
-//             if(curr.left != null) queue.offer(curr.left);
-//             if(curr.right != null) queue.offer(curr.right);
-//         }
-//         return res;
