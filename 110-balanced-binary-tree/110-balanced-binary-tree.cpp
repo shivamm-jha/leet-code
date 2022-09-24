13081361423922fb42ec26e4fb772e8ebcb46cb0
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-09-25 00:46:30 +0530

    Time: 19 ms (66.54%), Space: 21 MB (26.01%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -12,38 +12,21 @@
 class Solution {
 public:
     int height(TreeNode * root){
-        if(root==NULL){
-            return 0;
-        }
+        if(root==NULL)return 0;
         
-        int leftHeight = height(root->left);
-        if(leftHeight==-1) return -1;
-        int rightHeight = height(root->right);
-        if(rightHeight==-1) return -1;
-        if(abs(leftHeight-rightHeight)>1) return -1;
-        return max(leftHeight,rightHeight) +1;
+        return max(1+height(root->left), 1+ height(root->right));
+    }
+    
+    bool solve(TreeNode * root){
+        if(root==NULL) return true;
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        if(abs(left-right)>1)return false;
+        return solve(root->left) && solve(root->right);
     }
     bool isBalanced(TreeNode* root) {
-//          T.c= 0(N^2) bcoz 0(N) for checking the height and 0(N) for traversal
-        
-//         if(root==NULL) return true;
-//         int leftHeight = height(root->left);
-//         int rightHeight = height(root->right);
-//         if(abs(leftHeight-rightHeight)>1) return false;
-//         bool left = isBalanced(root->left);
-//         bool right = isBalanced(root->right);
-//         if(!left || !right) return false;
-        
-//         return true;
-        
-        
-//          we can reduce it to 0(N) 
-//         The approach is a slight modification in the height function
-//         after finding the height of the left and right subtree 
-//          we will check if their absolute difference is less than equal to 1
-//          if not then there itself we will return -1
-        
-        return height(root)!=-1;
-        
+        return solve(root);
     }
 };
