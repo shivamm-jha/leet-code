00d814b8cf0589aff10c820621cd14bcebe86f4c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-04 18:16:10 +0530

    Time: 24 ms (30.13%), Space: 21.2 MB (74.14%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool dfs(TreeNode * root, int target){
-        if(root==NULL) return false;
+       if(root==NULL) return false;
+        
         if(root->left==NULL && root->right==NULL && target-root->val==0)return true;
-        int left = dfs(root->left,target-root->val);
-        int right = dfs(root->right,target-root->val);
+        
+        bool left = dfs(root->left,target-root->val);
+        bool right = dfs(root->right, target-root->val);
         return left || right;
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
