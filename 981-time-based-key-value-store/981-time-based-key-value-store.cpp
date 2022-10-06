374f5237c47f3a07c308c91bc25505f07036646c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-06 19:27:54 +0530

    Time: 1017 ms (5.00%), Space: 131.7 MB (9.13%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,21 +1,43 @@
 class TimeMap {
 public:
     map<string,map<int,string>> mp;
-    TimeMap() {
-        
-    }
+    
+ 
     
     void set(string key, string value, int timestamp) {
         mp[key][timestamp] = value;
     }
     
     string get(string key, int timestamp) {
-        for(int i = timestamp;i>=1;i--){
-            if(mp[key].find(i)!=mp[key].end()){
-                return mp[key][i];
-            }
+//          linear search..
+        
+        // for(int i = timestamp;i>=1;i--){
+        //     if(mp[key].find(i)!=mp[key].end()){
+        //         return mp[key][i];
+        //     }
+        // }
+        
+        
+//          we can optimise it by binary search..
+        
+         if (mp.find(key) == mp.end()) {
+            return "";
         }
         
-        return "";
+        auto it = mp[key].upper_bound(timestamp);
+        // If iterator points to first element it means, no time <= timestamp exists.
+        if (it == mp[key].begin()) {
+            return "";
+        }
+        
+        // Return value stored at previous position of current iterator.
+        return prev(it)->second;
+        
+        
+        
+        
+        
+        // return "";
+         
     }
 };
