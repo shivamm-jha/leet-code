c3ed4aaf41f5d8bb52579c11524a169ce39be6d6
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-07 21:39:37 +0530

    Time: 212 ms (37.71%), Space: 38.8 MB (58.39%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,20 +1,37 @@
 class MyCalendar {
 public:
     vector<pair<int,int>> v;
+    set<pair<int,int>> st;
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        if(v.size()==0){
-            v.push_back({start,end});
-            return true;
+//         if(v.size()==0){
+//             v.push_back({start,end});
+//             return true;
+//         }
+        
+//         for(int i=0;i<v.size();i++){
+//             if(start<v[i].second && end > v[i].first)return false;
+//         }
+        
+        
+//         v.push_back({start,end});
+//         return true;
+        
+        pair<int,int> event{start,end};
+        auto nextEvent = st.lower_bound(event);
+        
+        if(nextEvent!=st.end() && nextEvent->first < end)return false;
+        
+        if(nextEvent!=st.begin()){
+            auto prevEvent = prev(nextEvent);
+            if(prevEvent->second > start)return false;
         }
         
-        for(int i=0;i<v.size();i++){
-            if(start<v[i].second && end > v[i].first)return false;
-        }
-        v.push_back({start,end});
+        st.insert(event);
         return true;
+        
     }
 };
