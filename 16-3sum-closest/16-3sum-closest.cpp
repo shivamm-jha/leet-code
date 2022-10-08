b563d7acabdb9606e4cb379e9c8f6b723ddbc871
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-08 19:37:07 +0530

    Time: 358 ms (72.84%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -5,27 +5,13 @@ public:
          
         int ans = 1e6;
          for(int i=0;i<nums.size()-2;i++){
-             int first = nums[i];
-             
+
              int l = i+1, r= nums.size()-1;
              while(l<r){
-                 int sum = first + nums[l] + nums[r];
-                 if(sum==target){
-                     ans = sum;
-                     return ans;
-                 }
-                 else if(sum < target){
-                     if(abs(target-ans) > abs(target-sum)){
-                         ans = sum;
-                     }
-                     l++;
-                 }
-                 else{
-                     r--;
-                     if(abs(target-ans) > abs(target-sum)){
-                         ans = sum;
-                     }
-                 }
+                 int sum = nums[i] + nums[l] + nums[r];
+                 if(abs(sum-target) < abs(ans-target))ans= sum;
+                 if(sum < target) l++;
+                 else r--;
             }
          }
         
