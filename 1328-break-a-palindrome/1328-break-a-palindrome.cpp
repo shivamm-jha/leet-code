0439cccbc73969bf183965965a3b43f2b1dbb641
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-10 12:26:30 +0530

    Time: 4 ms (29.56%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,45 +3,53 @@ public:
     string breakPalindrome(string s) {
         int n = s.size();
         if(n==1)return "";
-        int flag=0;
+//         int flag=0;
+        
+//         for(int i=0;i<n/2;i++){
+//             if(s[i]!='a'){
+//                 s[i]='a';
+//                 flag=1; break;
+//             }
+            
+//         }
+        
+        
+//         if(!flag){
+//             if(n%2==0){
+//                 for(int i=n/2;i<n;i++){
+//                 if(s[i]!='a'){
+//                     s[i]='a';
+//                     flag=1; break;
+//                 }
+//             }
+//             }
+//             else {
+//                 for(int i=n/2+1;i<n;i++){
+//                 if(s[i]!='a'){
+//                     s[i]='a';
+//                     flag=1; break;
+//                 }
+//             }
+//             }
+//         }
+        
+        
+//         if(!flag){
+//             s[n-1]= s[0]+1;
+//         }
         
         for(int i=0;i<n/2;i++){
             if(s[i]!='a'){
                 s[i]='a';
-                flag=1; break;
-            }
-            
-        }
-        
-        
-        if(!flag){
-            if(n%2==0){
-                for(int i=n/2;i<n;i++){
-                if(s[i]!='a'){
-                    s[i]='a';
-                    flag=1; break;
-                }
-            }
-            }
-            else {
-                for(int i=n/2+1;i<n;i++){
-                if(s[i]!='a'){
-                    s[i]='a';
-                    flag=1; break;
-                }
-            }
+                return s;
             }
         }
         
         
-        if(!flag){
-            s[n-1]= s[0]+1;
-        }
-        
-        
+        s[n-1]='b';
         
        
         
-        return s;
+        return n< 2 ? "" : s;
     }
 };
