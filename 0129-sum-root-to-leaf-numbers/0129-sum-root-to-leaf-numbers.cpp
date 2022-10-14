2f5c48864edb8f2aa4aecec17a9828f915c6d811
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-15 00:51:18 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (20.31%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,21 +11,35 @@
  */
 class Solution {
 public:
+//     int solve(TreeNode* root, int val){
+//         //cout<<val<<endl;
+//         if(root==NULL)return 0;
+        
+//         if(root->left==NULL && root->right==NULL){
+//             return val*10+root->val;
+//         }
+        
+//         int left = solve(root->left,val*10+root->val);
+//         int right = solve(root->right,val*10+root->val);
+        
+//         return left+ right;
+//     }
+    
     int solve(TreeNode* root, int val){
-        //cout<<val<<endl;
         if(root==NULL)return 0;
         
-        if(root->left==NULL && root->right==NULL){
-            return val*10+root->val;
+        val = val*10+root->val;
+        
+        if(!root->left && !root->right){
+            return val;
         }
         
-        int left = solve(root->left,val*10+root->val);
-        int right = solve(root->right,val*10+root->val);
-        
-        return left+ right;
+        return solve(root->left,val) + solve(root->right,val);
     }
     int sumNumbers(TreeNode* root) {
-        if(!root->left && !root->right)return root->val;
-        return solve(root->left,root->val) + solve(root->right,root->val);
+        // if(!root->left && !root->right)return root->val;
+        // return solve(root->left,root->val) + solve(root->right,root->val);
+        
+        return solve(root,0);
     }
 };
