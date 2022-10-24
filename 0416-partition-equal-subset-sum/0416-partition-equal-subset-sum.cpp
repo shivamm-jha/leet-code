b014b8fdf617adb05b033c5471cc4aa593f5887b
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-10-24 13:40:23 +0530

    Time: 404 ms (52.87%), Space: 27.2 MB (44.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,11 +3,9 @@ public:
     int dp[201][20001];
     bool solve(vector<int>&nums, int idx, int sum){
         if(idx==nums.size() || sum<=0){
-            // if(sum==0)return true;
-            // return false;
             return sum ==0;
         }
-       // if(sum<0)return false;
+       
         if(dp[idx][sum]!=-1)return dp[idx][sum];
         
         bool take = solve(nums,idx+1,sum-nums[idx]);
@@ -29,34 +27,4 @@ public:
 };
 
 
-
-//     int dp[201][20001];
-//     bool solve(vector<int> &nums, int n, int sum)
-//     {
-//         if (n <= 0 || sum <= 0)
-//             return sum == 0;
-        
-//         if (dp[n][sum] != -1)
-//             return dp[n][sum];
-        
-//         if (nums[n-1] > sum)
-//             return dp[n][sum] = solve(nums, n-1, sum);
-//         else
-//             return dp[n][sum] = solve(nums, n-1, sum) || solve(nums, n-1, sum-nums[n-1]);
-//     }
-    
-//     bool canPartition(vector<int>& nums) 
-//     {
-//         int sum = 0;
-//         memset(dp, -1, sizeof(dp));
-        
-//         for(int i = 0; i < nums.size(); i++)
-//             sum += nums[i];
-        
-//         if (sum % 2 != 0) 
-//             return false;
-        
-//         return solve(nums, nums.size(), sum/2);
-//     }
-// };
 
