69c370ee225c4daea7a800a868de78f026844df1
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2022-11-01 11:36:44 +0530

    Time: 58 ms (43.94%), Space: 13.3 MB (19.11%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,63 +1,38 @@
 class Solution {
 public:
-    vector<int> findBall(vector<vector<int>>& grid) {
-//         int n = nums.size();
-//         int m = nums[0].size();
+    vector<int> findBall(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int m = nums[0].size();
         
-//         vector<int> ans;
-        
-//         for(int j=0;j<n;j++){
-//             int row = 0, col =j;
-//             while(row<n){
-//                 if(nums[row][col]==1){
-//                     if(col+1>=m || nums[row][col+1]==-1){
-//                         ans.push_back(-1);
-//                         break;
-//                     }
-//                     col++;
-//                 }
-//                 else{
-//                     if(col-1<0 || nums[row][col-1]==1){
-//                         ans.push_back(-1);
-//                         break;
-//                     }
-//                     col--;
-//                 }
-//                 row++;
-//             }
-//             if(row==n){
-//                 ans.push_back(col);
-
-//             }
-//         }
-        
-//         return ans;
-         int m = grid.size();
-        int n = grid[0].size();
         vector<int> ans;
-        for (int j = 0; j < n; j++) {
-            int row = 0, col = j;
-            while (row < m) {
-                if (grid[row][col] == 1) {
-                    if (col+1 >= n || grid[row][col+1] == -1) {
+        
+        for(int j=0;j<m;j++){
+            int row = 0, col =j;
+            while(row<n){
+                if(nums[row][col]==1){
+                    if(col+1>=m || nums[row][col+1]==-1){
                         ans.push_back(-1);
                         break;
                     }
                     col++;
-                } else if (grid[row][col] == -1) {
-                    if (col-1 < 0 || grid[row][col-1] == 1) {
+                }
+                else{
+                    if(col-1<0 || nums[row][col-1]==1){
                         ans.push_back(-1);
                         break;
                     }
                     col--;
                 }
-				row++;
+                row++;
             }
-            if (row == m) {
+            if(row==n){
                 ans.push_back(col);
+
             }
         }
+        
         return ans;
+        
     }
 };
 
