ee4ef92663a7ca181b01d730b0185b3e9180ab3c
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2023-03-10 21:59:33 +0530

    Added solution - LeetHub

--- Maximum Triplet product - GFG/maximum-triplet-product.cpp
+++ Maximum Triplet product - GFG/maximum-triplet-product.cpp
@@ -8,18 +8,93 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+// class Solution {
+//   public:
+//     long long maxTripletProduct(long long arr[], int n)
+//     {
+//     	// Complete the function
+//     // 	sort(arr,arr+n);
+    	
+//     // 	long long ans1 = arr[n-3]*arr[n-2]*arr[n-1];
+    	
+//     // 	long long ans2 = arr[0]*arr[1]*arr[n-1];
+    	
+//     // 	return max(ans1,ans2);
+    
+//         long long maxx=LLONG_MIN, second_max=LLONG_MIN, third_max = LLONG_MIN;
+//         long long min=LLONG_MAX,second_min=LLONG_MAX;
+        
+//         for(int i=0;i<n;i++){
+//             int el = arr[i];
+//             if(el>maxx){
+//                 third_max=second_max;
+//                 second_max=maxx;
+//                 maxx=el;
+//             }
+//             else if(el>second_max){
+//                 third_max=second_max;
+//                 second_max=el;
+//             }
+//             else if(el>third_max){
+//                 el=third_max;
+//             }
+            
+//             if(el<min){
+//                 second_min=min;
+//                 min=el;
+//             }
+//             else if(el<second_min){
+//                 second_min=el;
+//             }
+//         }
+        
+//         // cout<<maxx<<" "<<second_max<<" "<<third_max<<endl;
+//         // cout<<min<<" "<<second_min;
+        
+//         if(min!=LLONG_MAX and second_min!=LLONG_MAX)
+        
+//         return max(maxx*second_max*third_max, min*second_min*maxx);
+        
+        
+//         return maxx * second_max * third_max;
+        
+//     }
+// };
+
+
 class Solution {
-  public:
-    long long maxTripletProduct(long long arr[], int n)
-    {
-    	// Complete the function
-    	sort(arr,arr+n);
-    	
-    	long long ans1 = arr[n-3]*arr[n-2]*arr[n-1];
-    	
-    	long long ans2 = arr[0]*arr[1]*arr[n-1];
-    	
-    	return max(ans1,ans2);
+public:
+    long long maxTripletProduct(long long arr[], int n) {
+        long long largest = LLONG_MIN, slargest = LLONG_MIN, tlargest = LLONG_MIN;
+        long long smallest = LLONG_MAX, ssmallest = LLONG_MAX;
+
+        for (int i = 0; i < n; i++) {
+            long long el = arr[i];
+
+            if (el > largest) {
+                tlargest = slargest;
+                slargest = largest;
+                largest = el;
+            } else if (el > slargest) {
+                tlargest = slargest;
+                slargest = el;
+            } else if (el > tlargest) {
+                tlargest = el;
+            }
+
+            if (el < smallest) {
+                ssmallest = smallest;
+                smallest = el;
+            } else if (el < ssmallest) {
+                ssmallest = el;
+            }
+        }
+
+        if (smallest != LLONG_MAX && ssmallest != LLONG_MAX) {
+            return max(smallest * ssmallest * largest, largest * slargest * tlargest);
+        }
+
+        return largest * slargest * tlargest;
     }
 };
 
